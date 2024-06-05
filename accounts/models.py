7210ed36b6f2211e793e3acd25baa3cea7bc6392
Riya Khandelwal <riyakhandelwal@jklu.edu.in>
2024-06-05 18:54:54 +0530

    feature/LinkedIn_Oauth

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ProfileLinkedin(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, default='', blank=True, null=True)
+    email = models.EmailField(default='')
+    # linkedin_profile_url = models.URLField(blank=True)
+
+    def __str__(self):
+         return f"LinkedIn Profile of {self.user.username}"
+
+
--- placement_cell_be/settings.py
+++ placement_cell_be/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+
+
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,15 +35,17 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
+    'social_django',
+    "accounts",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
 ]
 
 MIDDLEWARE = [
@@ -51,7 +56,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "social_django.middleware.SocialAuthExceptionMiddleware",
 ]
+
 
 ROOT_URLCONF = "placement_cell_be.urls"
 
@@ -66,10 +73,13 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'social_django.context_processors.backends',  
+                'social_django.context_processors.login_redirect', 
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = "placement_cell_be.wsgi.application"
 
@@ -84,6 +94,7 @@ DATABASES = {
         "PORT": 5432,
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
+        
     }
 }
 
@@ -128,3 +139,43 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.linkedin.LinkedinOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+
+SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY=os.environ.get('LINKEDIN_CLIENT_ID')
+SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET=os.environ.get('LINKEDIN_CLIENT_SECRET')
+LINKEDIN_REDIRECT_URI='http://localhost:8000/linkedin/callback'
+
+LOGIN_URL = 'signin'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'signin'
+
+
+
+SOCIAL_AUTH_LINKEDIN_OAUTH2_SCOPE = [
+    'w_member_social', 
+    'openid',
+    'profile',
+    'email'
+]
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'myapp.pipeline.require_email',  
+    'social_core.pipeline.partial.save_status_to_session',  
+)
